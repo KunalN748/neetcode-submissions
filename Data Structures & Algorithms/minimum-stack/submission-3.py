1782c1ac34a3d86d45d9eba1c263fdf6cb3edcc8
Kunal Nandawat <kunalnandawat9@gmail.com>
2026-07-18 20:56:49 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if not self.stack:
+            raise IndexError("pop from empty stack")
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        if not self.stack:
+            raise IndexError("top from empty stack")
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        if not self.min_stack:
+            raise IndexError("nothing in stack")
+        return self.min_stack[-1]
